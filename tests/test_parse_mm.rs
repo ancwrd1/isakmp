c6cfd6e73ab94b33b4cd4bd87f5c66b9a8fab241
Pankratov Dmitry <dmitry@pankratov.net>
2024-03-09 18:21:47 +0100

    Compilation fix

--- tests/test_parse_mm.rs
+++ tests/test_parse_mm.rs
@@ -1,13 +1,13 @@
 use std::io::Cursor;
 
-use isakmp::{message::IsakmpMessage, session::Ikev1Session};
+use isakmp::{message::IsakmpMessage, model::Identity, session::Ikev1Session};
 
 const DATA: &[u8] = include_bytes!("mm.bin");
 
 #[test]
 fn test_parse_main_mode() {
     let mut reader = Cursor::new(DATA);
-    let mut session = Ikev1Session::new().unwrap();
+    let mut session = Ikev1Session::new(Identity::None).unwrap();
     let msg = IsakmpMessage::parse(&mut reader, &mut session).unwrap();
     println!("{:#?}", msg);
 }
